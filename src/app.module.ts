4838646499a7d7e3094ff5defe6e0f2d6f655596
dmitrii <dmitry.esin@gmail.com>
2020-11-14 12:36:02 +0300

    close #3 added: controllers, services, logger, error handling

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,34 @@
-import { Module } from '@nestjs/common';
-
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import * as helmet from 'helmet';
+// import * as csurf from 'csurf';
+import * as rateLimit from 'express-rate-limit';
+import { CookieParserMiddleware } from '@nest-middlewares/cookie-parser';
+import { logger } from './middlewares/logger.middleware';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
+import { TodosController } from './todos/todos.controller';
+
+const rateLimitMiddlewareOptions = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
 
 @Module({
   imports: [TodosModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        CookieParserMiddleware,
+        helmet(),
+        // TODO: resolver problem with csurf(): Error: misconfigured csrf
+        rateLimitMiddlewareOptions,
+        logger,
+      )
+      .forRoutes(TodosController);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './exceptions/all-exceptions.filter';
+
+//TODO: If we want to bind middleware to every registered route at once,
+// we can use the use() method that is supplied by the INestApplication instance:
+// const app = await NestFactory.create(AppModule);
+// app.use(logger);
+// await app.listen(3000);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +20,9 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
+  app.enableCors();
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
 
   await app.listen(3000);
 }
