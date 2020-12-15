7170b082cfe77ff1075024179ca94b058572af9c
dmitrii <dmitry.esin@gmail.com>
2020-12-15 11:49:59 +0300

    close #15 database connected

--- src/database.module.ts
+++ src/database.module.ts
@@ -7,21 +7,19 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('POSTGRES_HOST'),
-        port: configService.get('POSTGRES_PORT'),
-        username: configService.get('POSTGRES_USER'),
-        password: configService.get('POSTGRES_PASSWORD'),
-        database: configService.get('POSTGRES_DB'),
-        //TODO:!!! ?????
-        // entities: [__dirname + '/../**/*.entity.ts'],
-        synchronize: true,
-        //TODO:!!! ?????
-        autoLoadEntities: true,
-        // //TODO:!!! Setting synchronize: true shouldn't be used in production - otherwise you can lose production data.
-        // synchronize: true,
-      }),
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get('POSTGRES_HOST'),
+          port: configService.get('POSTGRES_PORT'),
+          username: configService.get('POSTGRES_USER'),
+          password: configService.get('POSTGRES_PASSWORD'),
+          database: configService.get('POSTGRES_DB'),
+          // //TODO:!!! Setting synchronize: true shouldn't be used in production - otherwise you can lose production data.
+          synchronize: true,
+          autoLoadEntities: true,
+        };
+      },
     }),
   ],
 })
